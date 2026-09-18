f0cbf6fb845537d6991c011a541722f6911bfa3c
npp-prog <npp@mgocandoniaccounting.org>
2026-09-19 06:45:29 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
 // Copy the public web app config from Firebase Console > Project settings > Your apps.
 // Never put a service-account key or private key in this file.
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyBeIVtvzhHLDS6uWWo28lhUipZDBI5fZz0",
+  authDomain: "mgo-portal.firebaseapp.com",
+  projectId: "mgo-portal",
+  storageBucket: "mgo-portal.firebasestorage.app",
+  messagingSenderId: "385261655510",
+  appId: "1:385261655510:web:3e1085ba60ced96de90077"
 };
